de45dcfa0fffcc48eec2fd356fa6b6a25b02d6f3
Nagy János <nagy@zold.lan>
2025-09-12 10:31:02 +0200

    Komponensek létrehozva. Routing kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    // RouterOutlet, 
-    LoginComponent
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
